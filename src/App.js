44479cbe49253d8fe7da053ddcb7056d9a296f75
shanhan <thecsh3@gmail.com>
2020-05-01 16:30:38 +0800

    Init project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Client } from 'boardgame.io/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class GameBoard extends React.Component {
+  onClick(id) {
+    if (this.isActive(id)) {
+      this.props.moves.clickCell(id);
+      this.props.events.endTurn();
+    }
+  }
+
+  isActive(id) {
+    if (!this.props.isActive) return false;
+    if (this.props.G.cells[id] !== null) return false;
+    return true;
+  }
+
+  render() {
+    let winner = '';
+    if (this.props.ctx.gameover) {
+      winner =
+        this.props.ctx.gameover.winner !== undefined ? (
+          <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
+        ) : (
+          <div id="winner">Draw!</div>
+        );
+    }
+
+    const cellStyle = {
+      border: '1px solid #555',
+      width: '50px',
+      height: '50px',
+      lineHeight: '50px',
+      textAlign: 'center',
+    };
+
+    let tbody = [];
+    for (let i = 0; i < 18; i++) {
+      let cells = [];
+      for (let j = 0; j < 18; j++) {
+        const id = 18 * i + j;
+        cells.push(
+          <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
+            {this.props.G.cells[id]}
+          </td>
+        );
+      }
+      tbody.push(<tr key={i}>{cells}</tr>);
+    }
+
+    return (
+      <div>
+        <table id="board">
+          <tbody>{tbody}</tbody>
+        </table>
+        {winner}
+      </div>
+    );
+  }
 }
+
+function IsVictory(cells) {
+
+}
+
+function IsDraw(cells) {
+  
+}
+
+const NoClue = {
+  setup: () => ({ cells: Array(324).fill(null) }),
+
+  moves: {
+    clickCell: (G, ctx, id) => {
+      if (G.cells[id] === null) {
+        G.cells[id] = ctx.currentPlayer;
+      }
+    },
+  },
+
+  endIf: (G, ctx) => {
+    if (IsVictory(G.cells)) {
+      return { winner: ctx.currentPlayer };
+    }
+    if (IsDraw(G.cells)) {
+      return { draw: true };
+    }
+  },
+};
+
+const App = Client({ 
+  game: NoClue,
+  numPlayers: 6,
+  board: GameBoard
+});
 
 export default App;
