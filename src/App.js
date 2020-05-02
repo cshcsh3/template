05a6b365f5abb460707f2a5799376ba1a70fdc08
shanhan <thecsh3@gmail.com>
2020-05-02 15:33:05 +0800

    Initialize player positions
    Add dev dependencies
    
    Clean up project files

--- src/App.js
+++ src/App.js
@@ -1,97 +1,135 @@
-import React from 'react';
-import { Client } from 'boardgame.io/react';
+import React from 'react'
+import { Client } from 'boardgame.io/react'
 
 class GameBoard extends React.Component {
-  onClick(id) {
-    if (this.isActive(id)) {
-      this.props.moves.clickCell(id);
-      this.props.events.endTurn();
-    }
-  }
-
-  isActive(id) {
-    if (!this.props.isActive) return false;
-    if (this.props.G.cells[id] !== null) return false;
-    return true;
-  }
-
-  render() {
-    let winner = '';
-    if (this.props.ctx.gameover) {
-      winner =
-        this.props.ctx.gameover.winner !== undefined ? (
-          <div id="winner">Winner: {this.props.ctx.gameover.winner}</div>
-        ) : (
-          <div id="winner">Draw!</div>
-        );
+    onClick(id) {
+        if (this.isActive(id)) {
+            this.props.moves.clickCell(id)
+            this.props.events.endTurn()
+        }
     }
 
-    const cellStyle = {
-      border: '1px solid #555',
-      width: '50px',
-      height: '50px',
-      lineHeight: '50px',
-      textAlign: 'center',
-    };
-
-    let tbody = [];
-    for (let i = 0; i < 18; i++) {
-      let cells = [];
-      for (let j = 0; j < 18; j++) {
-        const id = 18 * i + j;
-        cells.push(
-          <td style={cellStyle} key={id} onClick={() => this.onClick(id)}>
-            {this.props.G.cells[id]}
-          </td>
-        );
-      }
-      tbody.push(<tr key={i}>{cells}</tr>);
+    isActive(id) {
+        if (!this.props.isActive) return false
+        if (this.props.G.cells[id] !== null) return false
+        return true
     }
 
-    return (
-      <div>
-        <table id="board">
-          <tbody>{tbody}</tbody>
-        </table>
-        {winner}
-      </div>
-    );
-  }
+    render() {
+        const { playerPosition, cells } = this.props.G
+        let winner = ''
+        if (this.props.ctx.gameover) {
+            winner =
+                this.props.ctx.gameover.winner !== undefined ? (
+                    <div id="winner">
+                        Winner: {this.props.ctx.gameover.winner}
+                    </div>
+                ) : (
+                    <div id="winner">Draw!</div>
+                )
+        }
+
+        const cellStyle = {
+            border: '1px solid #555',
+            width: '50px',
+            height: '50px',
+            lineHeight: '50px',
+            textAlign: 'center',
+            backgroundColor: '#333',
+        }
+
+        let tbody = []
+        for (let i = 0; i < 18; i++) {
+            let tCells = []
+            for (let j = 0; j < 18; j++) {
+                const id = 18 * i + j
+                let skip = false
+
+                for (let key in playerPosition) {
+                    if (playerPosition.hasOwnProperty(key)) {
+                        if (id === playerPosition[key]) {
+                            const playerStyle = {
+                                color: key,
+                                fontWeight: 'bold',
+                            }
+                            tCells.push(
+                                <td
+                                    style={cellStyle}
+                                    key={id}
+                                    onClick={() => this.onClick(id)}
+                                >
+                                    <span style={playerStyle}>P</span>
+                                </td>
+                            )
+                            skip = true
+                            break
+                        }
+                    }
+                }
+
+                if (skip) {
+                    continue
+                }
+
+                tCells.push(
+                    <td
+                        style={cellStyle}
+                        key={id}
+                        onClick={() => this.onClick(id)}
+                    >
+                        {cells[id]}
+                    </td>
+                )
+            }
+            tbody.push(<tr key={i}>{tCells}</tr>)
+        }
+
+        return (
+            <div>
+                <table id="board">
+                    <tbody>{tbody}</tbody>
+                </table>
+                {winner}
+            </div>
+        )
+    }
 }
 
-function IsVictory(cells) {
+function IsVictory(cells) {}
 
-}
-
-function IsDraw(cells) {
-  
-}
+function IsDraw(cells) {}
 
 const NoClue = {
-  setup: () => ({ cells: Array(324).fill(null) }),
+    setup: () => ({
+        cells: Array(324).fill(null),
+        playerPosition: {
+            red: 311,
+            yellow: 216,
+            white: 6,
+            blue: 11,
+            green: 179,
+            purple: 251,
+        },
+    }),
 
-  moves: {
-    clickCell: (G, ctx, id) => {
-      if (G.cells[id] === null) {
-        G.cells[id] = ctx.currentPlayer;
-      }
+    moves: {
+        clickCell: (G, ctx, id) => {},
     },
-  },
 
-  endIf: (G, ctx) => {
-    if (IsVictory(G.cells)) {
-      return { winner: ctx.currentPlayer };
-    }
-    if (IsDraw(G.cells)) {
-      return { draw: true };
-    }
-  },
-};
+    endIf: (G, ctx) => {
+        if (IsVictory(G.cells)) {
+            return { winner: ctx.currentPlayer }
+        }
+        if (IsDraw(G.cells)) {
+            return { draw: true }
+        }
+    },
+}
 
-const App = Client({ 
-  game: NoClue,
-  numPlayers: 6,
-  board: GameBoard
-});
+const App = Client({
+    game: NoClue,
+    numPlayers: 6,
+    board: GameBoard,
+})
 
-export default App;
+export default App
